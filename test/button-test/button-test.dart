032bb1bcd7b79832d3d0123b5a91d4665f91fa4e
danielVg <danielveragi@gmail.com>
2019-05-19 20:24:01 +0200

    Tests now only in one file

--- test/button-test/button-test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import './test-button.dart';
-
-void main() {
-  testWidgets('Test the button', (WidgetTester tester) async {
-    await tester.pumpWidget(TestButton());
-  });
-}
--- test/button_test.dart
+++ test/button_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_template/widgets/app_button/index.dart';
 
@@ -10,4 +11,10 @@ class TestButton extends StatelessWidget {
       home: AppButton(buttonName: "Test", onPressed: null, buttonTextStyle: null,)
     );
   }
+}
+
+void main() {
+  testWidgets('Test the button', (WidgetTester tester) async {
+    await tester.pumpWidget(TestButton());
+  });
 }
